4770e019d9becf8a4dd3185b6982dc2ff0064bb7
MatthieuMothay <matthieu.mothay@etu.univ-tlse2.fr>
2015-12-03 23:22:11 +0100

    Premiere vue

---
+++ VIEW/include/evenements.php
@@ -0,0 +1,5 @@
+﻿<div class="col-lg-2" id="droite">
+				<div id="even">
+					Evenements à venir
+				</div>
+			</div>
---
+++ VIEW/include/footer.php
@@ -0,0 +1,20 @@
+<!-- Bandeau inf --> 
+
+			<div class="col-lg-12" id="foot">
+				<div class="col-lg-2" id="adr">
+					Médiathèque Municipale de Blagnac</br>
+					15, rue des Pommes</br>
+					31700 BLAGNAC
+				</div>		
+				<div class="col-lg-8">
+					<br/>All rights reserved ©
+				</div>
+				<div class="col-lg-2">
+					tel : 05.07.08.10.11 <br/>
+					mail : gestMed@gmail.com
+				</div>
+			</div>
+
+    </div>
+  </body>
+</html>
