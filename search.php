42dc34c6bde5ca0ba5f4eb0c4c78ce87b37fcbd0
PalhiereAymeric <aymericpalhiere@gmail.com>
2015-12-04 06:31:20 +0100

    mouais

--- search.php
+++ search.php
@@ -2,7 +2,8 @@
   header('Location :index.php?msg=Vous devez saisir un mot-clé');
 }
 include('connect.inc.php');
-$requete = $bdd->prepare('SELECT * from tweet WHERE motcle LIKE \'%?%\' ORDER BY date LIMIT 0,10');
+echo "bonjour";
+$requete = $bdd->prepare('SELECT * from tweet WHERE mots_cles LIKE \'%?%\' ORDER BY date LIMIT 0,10');
 $requete->execute(array(htmlspecialchars($_POST['recherche'])));
 while($donnees = $requete -> fetch()){
   foreach($donnees as $tweet){
@@ -30,16 +31,28 @@ while($donnees = $requete -> fetch()){
 				<div class="row">
 					<div class= "col-lg-offset-6 col-lg-6">
 						<ul class="nav navbar-nav">
-							<li class="active"><a href="#">Accueil</a></li>
+							<li><a href="index.php">Accueil</a></li>
 							<li><a href="salon.php">Salons</a></li>
 							<li><a href="map.php">Map</a></li>
 						</ul>
 					</div>
 				</div>
-				
+        <div class="row">
+          <form method="Post" action="search.php">
+					<div class="form-group">
+						<div class="input-group input-group-lg icon-addon addon-lg">
+							<input type="text" placeholder="Saisissez des mots-clés" name="recherche" id="schbox" class="form-control input-lg">
+							<i class="icon icon-search"></i>
+							<span class="input-group-btn">
+								<button type="submit" class="btn btn-inverse">Rechercher</button>
+							</span>
+						</div>
+					</div>
+        </form>
+      </div>
 				<div class="row">
 					<div class= "col-lg-offset-1 col-lg-10" id="blocnews">
-						<?php echo $tabTweets ;
+						<?php print_r($tabTweets); ?>
 					</div>
 				</div>
 			</div>
