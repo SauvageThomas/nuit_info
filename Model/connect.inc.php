592ab0a73d5a2f62f955118ab606170f8cff58ff
Thomas <sauvage.thomas@gmail.com>
2015-12-04 02:06:02 +0100

    bdd

--- Model/connect.inc.php
+++ Model/connect.inc.php
@@ -2,7 +2,7 @@
   $log = 'u694104836_ts';
   $mdp = 'nuitinfo';
   try{
-    $conn = new PDO('mysql:localhost;dbname=u694104836_base0;charset=UTF8', $log, $mdp); 
+    $bdd = new PDO('mysql:localhost;dbname=u694104836_base0;charset=UTF8', $log, $mdp); 
     }
     catch (PDOException $e){
       echo "Erreur : ".$e->getMessage();
