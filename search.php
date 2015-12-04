d49ff9c795893cc5acb2e748ac325ed6af2747fe
Thomas <sauvage.thomas@gmail.com>
2015-12-04 03:24:56 +0100

    works

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php if((!isset($_POST['recherche'])) || $_POST['recherche']==null){
   header('Location :index.php?msg=Vous devez saisir un mot-clé');
 }
-include('Model/connect.inc.php');
+include('connect.inc.php');
 $requete = $bdd->prepare('SELECT * from tweet WHERE motcle LIKE \'%?%\' ORDER BY date LIMIT 0,10');
 $requete->execute(array(htmlspecialchars($_POST['recherche'])));
 while($donnees = $requete -> fetch()){
