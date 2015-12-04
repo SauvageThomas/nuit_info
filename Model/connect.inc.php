5e3ee84fe09e6b9cb20d41efcecb4936dd04e2c4
Thomas <sauvage.thomas@gmail.com>
2015-12-04 02:47:59 +0100

    test

--- Model/connect.inc.php
+++ Model/connect.inc.php
@@ -1,11 +1,10 @@
 <?php
-  $log = 'u694104836_ts';
-  $mdp = 'nuitinfo';
-  try{
-    $bdd = new PDO('mysql:localhost;dbname=u694104836_base0;charset=UTF8', $log, $mdp); 
-    }
-    catch (PDOException $e){
-      echo "Erreur : ".$e->getMessage();
-      die();
-    }
+$log = 'u694104836_ts';
+$mdp = 'nuitinfo';
+try {
+	$bdd = new PDO ( 'mysql:host=localhost;dbname=u694104836_base0;charset=UTF8', $log, $mdp );
+} catch ( PDOException $e ) {
+	echo "Erreur : " . $e->getMessage ();
+	die ();
+}
 ?>
--- VIEW/salon.php
+++ VIEW/salon.php
@@ -1,21 +1,22 @@
- <!DOCTYPE html>
+
+<!DOCTYPE html>
 <html>
-  <head>
-  <meta charset="utf-8">
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container-fluid">
+<head>
+<meta charset="utf-8">
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container-fluid">
 
 
 
 		<!-- LOGO    SOMMAIRE-->
 		<div class="row">
 			<div class="col-lg-offset-3 col-lg-6">
-				 </Br></Br></Br>
+				</Br> </Br> </Br>
 				<div class="row">
-					<div class= "col-lg-offset-6 col-lg-6">
+					<div class="col-lg-offset-6 col-lg-6">
 						<ul class="nav navbar-nav">
 							<li class="active"><a href="index.php">Accueil</a></li>
 							<li><a href="#">Salons</a></li>
@@ -26,32 +27,37 @@
 				<div class="row">
 					<div class="form-group">
 						<div class="input-group input-group-lg icon-addon addon-lg">
-							<input type="text" placeholder="Saisissez des mots-clés" name="" id="schbox" class="form-control input-lg">
-							<i class="icon icon-search"></i>
-							<span class="input-group-btn">
+							<input type="text" placeholder="Saisissez des mots-clés" name=""
+								id="schbox" class="form-control input-lg"> <i
+								class="icon icon-search"></i> <span class="input-group-btn">
 								<button type="submit" class="btn btn-inverse">Rechercher</button>
 							</span>
 						</div>
 					</div>
 				</div>
 				<div class="row">
-					<div class= "col-lg-offset-1 col-lg-10" id="blocnews">
-						LISTES DES SALONS
+					<div class="col-lg-offset-1 col-lg-10" id="blocnews">
+						<?php
+						include '../Model/connect.inc.php';
+						$query = $bdd->query ( "SELECT * FROM salon" );
+						echo 'mdr';
+						while ( $line = $query->fetch () ) {
+							echo $line ['nom'] . '<br>';
+						}
+						
+						?>
 					</div>
 				</div>
 			</div>
-			<div class="col-lg-offset-1 col-lg-2" id="discutinstant">
-				Chat instant'
-		 
-			</div>
-		 </div>
-		 
-		 
+			<div class="col-lg-offset-1 col-lg-2" id="discutinstant">Chat
+				instant'</div>
 		</div>
-		</body>
-		 
+
+
+	</div>
+</body>
+
 </html>
-		 
-		 
-		 
-		 
+
+
+
